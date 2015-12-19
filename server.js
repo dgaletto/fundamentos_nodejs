5cbddcaa26bef8be5e4635256d1c67ace4c5270a
Damian Galetto <damian@prixmates.com>
2015-12-19 15:48:46 -0300

    Variables de entorno

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 'use strict';
-var http = require('http');
+var http = require('http'),
+	port = process.env.port || 8080;
 var server = http.createServer(function(req, res){
 	console.log('server.js: Estan haciendo una peticion');
 });
-server.on('request', function(req, res){
-	console.log('server.js: Estan haciendo una peticion');
-});
-server.listen(8080, function(){
-	console.log('server.js: Inicializa el server en el puerto 8080');
+server.listen(port, function(){
+	console.log('server.js: Inicializa el server en el puerto ' + port);
 });
