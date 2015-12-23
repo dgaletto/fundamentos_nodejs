9765df7dd4ed77cce90fb97f1b4fa96c28ad01ef
Damian Galetto <damian@prixmates.com>
2015-12-22 22:33:11 -0300

    homa manola

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ var http = require('http'),
 	port = process.env.port || 8080;
 var server = http.createServer(function(req, res){
 	console.log('server.js: Estan haciendo una peticion');
+	res.writeHead(200, {'content_type': 'text/plain'});
+	res.end('Hola manola!');
 });
 server.listen(port, function(){
 	console.log('server.js: Inicializa el server en el puerto ' + port);
